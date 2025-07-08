f8eece5271d9f3345aa6cb22458a50b11e0be0b4
Aleksey Pavlovskii <agp251093@gmail.com>
2025-07-08 20:25:57 +0300

    add functions in widget.py

---
+++ srs/widget.py
@@ -0,0 +1,23 @@
+def mask_account_card(numbers_card_or_check: str) -> str:
+    """Функция, которая маскирует номер карты или счета"""
+
+    card_name = []
+    card_namber_split = numbers_card_or_check.split(" ")
+    join_card_number = "".join(card_namber_split[-1])
+    for i in card_namber_split:
+        if i == "Счет":
+            card_name.append(i)
+            return f"{" ".join(card_name)} **{join_card_number[-4:]}"
+        for i in card_namber_split:
+            if i.isalpha():
+                card_name.append(i)
+        return f"{" ".join(card_name)} {join_card_number[0:4]} {join_card_number[4:6]}** **** {join_card_number[-4:]}"
+
+
+def get_date(user_data: str) -> str:
+    """Функция, которая принимает дату пользователя и возвращает дату в формате 'ДД.ММ.ГГГГ'"""
+
+    data_part = user_data.split("T")[0]
+    times = data_part.split("-")[::-1]
+    format_data = ".".join(times)
+    return format_data
