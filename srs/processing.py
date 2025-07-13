4bd413eecc67723a8e45af8f6dd8f516da7aa25d
Aleksey Pavlovskii <agp251093@gmail.com>
2025-07-13 14:57:07 +0300

    add filer by state func

---
+++ srs/processing.py
@@ -0,0 +1,12 @@
+from typing import Any, Dict, List
+
+
+def filter_by_state(list_dic: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+    """Функция, которая принимает список словарей и возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению"""
+
+    filter_state = []
+    for dic in list_dic:
+        if dic.get("state") == state:
+            filter_state.append(dic)
+    return filter_state
