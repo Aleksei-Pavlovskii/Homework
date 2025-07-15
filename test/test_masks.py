de69e0d36dd754531f3219b926cb7df73f3d5ca9
Aleksey Pavlovskii <agp251093@gmail.com>
2025-07-15 23:50:19 +0300

    add test masks

---
+++ test/test_masks.py
@@ -0,0 +1,14 @@
+from srs.masks import get_mask_card_number, get_mask_account
+
+
+def test_get_mask_card_number():
+    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
+    assert get_mask_card_number("7000792289601") == "Некоректный номер карты"
+    assert get_mask_card_number("") == "Некоректный номер карты"
+
+
+def test_get_mask_account():
+    assert get_mask_account("73654108430135874305") == "**4305"
+    assert get_mask_account("4305") == "**4305"
+    assert get_mask_account("305") == "Некоректный номер счёта"
+    assert get_mask_account("") == "Некоректный номер счёта"
