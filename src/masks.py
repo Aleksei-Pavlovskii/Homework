2d844e8b8151824448d7e165e34a860ce0267e3c
Aleksey Pavlovskii <agp251093@gmail.com>
2025-08-12 21:35:55 +0300

    fixes

--- src/masks.py
+++ src/masks.py
@@ -1,35 +1,38 @@
 import logging
 
-logger = logging.getLogger('masks')
+logger = logging.getLogger("masks")
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('logs/masks.log', mode='w', encoding='utf-8')
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler = logging.FileHandler("logs/masks.log", mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
+
 
 def get_mask_card_number(card_number: str) -> str:
     """Функция, которая принимает номер карты и возвращает маску номера"""
     try:
         if len(card_number) == 16:
-            logger.info(f' Пользователь ввёл номер карты {card_number}')
+            logger.info(f" Пользователь ввёл номер карты {card_number}")
             return f"{str(card_number)[0:4]} {str(card_number)[4:6]}** **** {str(card_number)[12:]}"
         else:
-            logger.warning(f'{card_number} - некорректный номер карты')
+            logger.warning(f"{card_number} - некорректный номер карты")
             return "Некорректный номер карты"
     except Exception as err:
         logger.setLevel(logging.ERROR)
-        logger.error(f'Произошла ошибка {err}')
+        logger.error(f"Произошла ошибка {err}")
+        return 'Ошибка при обработке номера карты'
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция, которая принимает номер счета и возвращает маску номера"""
     try:
-        logger.info(f' Пользователь ввёл номер счета {account_number}')
+        logger.info(f" Пользователь ввёл номер счета {account_number}")
         if len(account_number) >= 4:
             return f"**{str(account_number)[-4:]}"
         else:
-            logger.warning(f'{account_number} - некорректный номер счета')
+            logger.warning(f"{account_number} - некорректный номер счета")
             return "Некорректный номер счёта"
     except Exception as err:
         logger.setLevel(logging.ERROR)
-        logger.error(f'Произошла ошибка {err}')
+        logger.error(f"Произошла ошибка {err}")
+        return 'Ошибка при обработке номера счета'
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,20 @@
 import json
+import logging
 import os
 from typing import Any
 
-import logging
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
-logger = logging.getLogger('utils')
+logger = logging.getLogger("utils")
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('logs/utils.log', mode='w', encoding='utf-8')
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler = logging.FileHandler("logs/utils.log", mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
+
 
 def read_json_file(filename: str | None = None) -> Any | list:
     """Функция, которая проверяет наличие файла и читает его"""
@@ -21,21 +22,22 @@ def read_json_file(filename: str | None = None) -> Any | list:
         if filename and os.path.exists(filename):
             with open(filename, encoding="utf-8") as f:
                 data = json.load(f)
-            logger.info(f'Файл {filename} найден')
+            logger.info(f"Файл {filename} найден")
             return data
         else:
-            logger.warning(f'Файл {filename} не найден')
+            logger.warning(f"Файл {filename} не найден")
             return []
     except Exception as err:
         logger.setLevel(logging.ERROR)
-        logger.error(f'Произошла ошибка {err}')
+        logger.error(f"Произошла ошибка {err}")
+        return 'Произошла ошибка'
 
 
 def currency_conversion(transactions: list[dict]) -> float:
     """Функция, которая конвертирует валюту и возвращает сумму операций"""
     amount = []
     try:
-        logger.info('Запущен процесс конвертации валюты в транзакциях')
+        logger.info("Запущен процесс конвертации валюты в транзакциях")
         for transaction in transactions:
             trans_code = transaction["operationAmount"]["currency"]["code"]
             if trans_code == "RUB":
@@ -51,10 +53,12 @@ def currency_conversion(transactions: list[dict]) -> float:
 
                 results = response.json()
                 amount.append(float(results["result"]))
-        logger.info(f'Процесс завершен, сумма транзакций {round(sum(amount), 2)}')
+        logger.info(f"Процесс завершен, сумма транзакций {round(sum(amount), 2)}")
         return round(sum(amount), 2)
     except Exception as err:
         logger.setLevel(logging.ERROR)
-        logger.error(f'Произошла ошибка {err}')
+        logger.error(f"Произошла ошибка {err}")
+        return 'Ошибка при обработке транзакций'
+
 
 # print(currency_conversion(read_json_file("../data/operations.json")))
